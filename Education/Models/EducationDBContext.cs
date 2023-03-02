cc93a6f757675cc39ae1dbd1bb8959f8e57779e0
Rob Jaindl <robmax2023@outlook.com>
2023-03-02 09:51:32 -0500

    Add project files.

---
+++ Education/Models/EducationDBContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Education.Models {
+    public class EducationDBContext : DbContext {
+        public DbSet<Major> Majors { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+    public EducationDBContext(DbContextOptions<EducationDBContext> options) : base(options) { }
+    }
+}
